aeee339d4cce82fef3660fd8e69e9d1146ae4877
goncalhurtado <gonzalomilano@gmail.com>
2023-09-04 18:43:07 -0300

    create  card form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import CardCreator from "./components/CardCreator";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./sass/main.scss";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <CardCreator />;
+  return (
+    <div className="d-flex justify-content-center">
+      <CardCreator />
+    </div>
+  );
 }
 
 export default App;
--- src/components/CardCreator.jsx
+++ src/components/CardCreator.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const CardCreator = () => {
+  const [dataForm, setDataForm] = useState({});
+
+  const handleChange = (e) => {
+    setDataForm({ ...dataForm, [e.target.name]: e.target.value });
+  };
+  //   console.log(dataForm);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes enviar los datos del formulario a donde los necesites
+    console.log(dataForm);
+  };
+
   return (
-    <div class="card w-25">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Button
-        </a>
-      </div>
+    <div className="card w-25 mt-5">
+      <form onSubmit={handleSubmit}>
+        <div className="card-body">
+          <div className="card-title">
+            <input
+              name="cardTitle"
+              className="form-control"
+              type="text"
+              placeholder="Add title here"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="card-text mb-2">
+            <textarea
+              name="cardText"
+              className="form-control"
+              type="text"
+              placeholder="Add text here"
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <div className="">
+            <button type="submit" className="btn btn-primary">
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
