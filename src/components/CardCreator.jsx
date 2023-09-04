b7c5fd5a492f3937a980abda4b9e4dbfd91c48a5
goncalhurtado <gonzalomilano@gmail.com>
2023-09-04 17:28:51 -0300

    installed bootstrap,scss and other deps

---
+++ src/components/CardCreator.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const CardCreator = () => {
+  return (
+    <div class="card w-25">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          With supporting text below as a natural lead-in to additional content.
+        </p>
+        <a href="#" class="btn btn-primary">
+          Button
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CardCreator;
